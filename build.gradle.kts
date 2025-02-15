a6ccbd64e9798d3f29675ac348af365089580c41
Utko Andrey <utko.andrey@wb.ru>
2025-02-15 14:42:26 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.2.4")
+    implementation("io.ktor:ktor-server-netty:2.2.4")
+    implementation("io.ktor:ktor-server-content-negotiation:2.2.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.4")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
